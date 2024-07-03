0ef35664a72c4c29faaf685bea1a40519ac681c3
aoito <ao.ito@tabian.co>
2024-07-04 01:15:17 +0900

    💡 コメント

--- examples/playground/src/main.ts
+++ examples/playground/src/main.ts
@@ -1,9 +1,11 @@
 import { createApp } from "chibivue";
 
+// レンダリングする内容を定義
 const app = createApp({
   render() {
     return 'Hello world.'
   }
 })
 
+// DOMにマウントする
 app.mount('#app')
--- packages/index.ts
+++ packages/index.ts
@@ -9,7 +9,7 @@ export type App = {
 export const createApp = (options: Options): App => {
   return {
     mount: selector => {
-      const root = document.querySelector(selector)
+      const root = document.querySelector(selector) // selectorには'#app'を受け取る想定
       if (root) {
         root.innerHTML = options.render()
       }
